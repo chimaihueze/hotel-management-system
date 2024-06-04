455bb9ee3fd6307fa0474b983bb67ebc006d1420
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-06-04 09:20:25 +0100

    Upadated HR class with customer field

--- src/main/java/com/hms/hotel_management_system/entity/HR.java
+++ src/main/java/com/hms/hotel_management_system/entity/HR.java
@@ -14,6 +14,9 @@ public class HR {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column
+    private Customer customer;
+
     @OneToMany(mappedBy = "HR", fetch = FetchType.LAZY ,cascade = CascadeType.ALL)
     private List<Report> Reports;
 }

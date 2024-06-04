84a779bb69e3f36e80be8349a8f7c9492764a15c
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-06-04 08:43:40 +0100

    Added Bookingmanager entity

---
+++ src/main/java/com/hms/hotel_management_system/entity/BookingManager.java
@@ -0,0 +1,21 @@
+package com.hms.hotel_management_system.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "BOOKING_MANAGER")
+public class BookingManager {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private Customer customer;
+
+    @OneToMany(mappedBy = "bookingManager", fetch = FetchType.LAZY ,cascade = CascadeType.ALL)
+    private List<Booking> Bookings;
+}

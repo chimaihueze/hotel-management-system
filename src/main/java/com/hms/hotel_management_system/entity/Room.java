bb9541fd30361f2dc5358af42df0666fdb22a39b
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-06-11 09:00:31 +0100

    Added @NoAgrsConstructor, @AllArgsConstructor to Room class

--- src/main/java/com/hms/hotel_management_system/entity/Room.java
+++ src/main/java/com/hms/hotel_management_system/entity/Room.java
@@ -1,10 +1,14 @@
 package com.hms.hotel_management_system.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "ROOM")
 public class Room {
 

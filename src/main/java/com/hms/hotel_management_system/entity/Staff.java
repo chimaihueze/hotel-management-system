c42ebe425a7492806abdd38e419f958511ea707e
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-06-11 08:50:47 +0100

    Removed @Entity annotations on Staff class, Added @NoAgrsConstructor, @AllArgsConstructor

--- src/main/java/com/hms/hotel_management_system/entity/Staff.java
+++ src/main/java/com/hms/hotel_management_system/entity/Staff.java
@@ -1,16 +1,17 @@
 package com.hms.hotel_management_system.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "Staff")
 public class Staff extends User {
 
-    @Column
     private String department;
 
-    @Column
     private Double Salary;
 }
